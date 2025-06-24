f546a85f73327fe66f61b1482b7d7778971f4bc2
whlionel704 <whlionel704@gmail.com>
2025-06-24 15:24:35 +0800

    health

--- src/main/java/tech/learns/lionel/spiritual_health_tracker/Controllers/HealthController.java
+++ src/main/java/tech/learns/lionel/spiritual_health_tracker/Controllers/HealthController.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({ "", "/" })
+@RequestMapping()
 public class HealthController {
-    @GetMapping("/health")
-    public String getHealth() {
-        return "all good";
+
+    @GetMapping({ "/" })
+    public String getOk() {
+        return "all ok";
     }
 
-    @GetMapping("/test3")
-    public String getTestCiCd() {
-        return "ci/cd pipeline is working test3";
+    @GetMapping("/health")
+    public String getHealth() {
+        return "all good and healthy";
     }
 }
